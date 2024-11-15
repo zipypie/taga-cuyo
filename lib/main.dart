78357456df63ecd9784cc3c176ecda66a9b2776f
zipypie <201980170@psu.palawan.edu.ph>
2024-11-15 20:59:00 +0800

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,18 @@ Future<void> main() async {
   
   try {
     // Initialize Firebase
-    await Firebase.initializeApp();
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+      apiKey: "AIzaSyA_VoT0W3zD6u5GVEa24dLcMkt3DJtnqYg",
+      authDomain: "tagalog-cuyonon-mobile-app.firebaseapp.com",
+      databaseURL: "https://tagalog-cuyonon-mobile-app-default-rtdb.firebaseio.com",
+      projectId: "tagalog-cuyonon-mobile-app",
+      storageBucket: "tagalog-cuyonon-mobile-app.appspot.com",
+      messagingSenderId: "598478516019",
+      appId: "1:598478516019:web:e680422657a9ebde305846",
+      measurementId: "G-9R8PRFSXQ1",
+    ),
+    );
 
     // Initialize Hive
     final appDocumentDir = await getApplicationDocumentsDirectory();
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,7 +13,6 @@ import firebase_core
 import firebase_storage
 import path_provider_foundation
 import shared_preferences_foundation
-import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -24,5 +23,4 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
